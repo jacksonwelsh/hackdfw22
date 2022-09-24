eb0680ff872780f8061af6981462179b1c5769bd
Rocko Bishop <deck@RockoDeck.event.rightround.com>
2022-09-24 13:40:50 -0500

    Enhanced sim card look.

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ interface CardProps {
   className?: string;
 }
 
-const Card: React.FC<CardProps> = ({ children, className }) => {
+const Card: React.FC<CardProps> = ({ children, className}) => {
   return (
     <div className={"rounded-md border-gray-300 border-2 p-2 " + className}>
       {children}
--- pages/simulator.tsx
+++ pages/simulator.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Card from "../components/Card";
 
 const Simulator: NextPage = () => {
+  var savings = 0
+
   return (
     <main className="flex-col">
       <div className="text-center pb-10">
@@ -10,20 +12,21 @@ const Simulator: NextPage = () => {
           Explore various life events and experience how they may affect your retirement!
         </h2>
       </div>
-      <div className="grid grid-cols-2 gap-4 my-4 px-10 ">
-        <Card className="text-center border-2 border-violet-700">
+      <div className="grid grid-cols-2 gap-4 my-4 px-10 text-center">
+        <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
           <h2>Buy a Car.</h2>
         </Card>
-        <Card className="text-center border-2 border-violet-700">
-          <h2>Deposit into 401k.</h2>
+        <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
+          <h2>Deposit $200 into 401k.</h2>
         </Card>
-        <Card className="text-center border-2 border-violet-700">
+        <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
           <h2>Pay Credit Card debt.</h2>
         </Card>
-        <Card className="text-center border-2 border-violet-700">
+        <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
           <h2>Hire an Escort. (sus)</h2>
         </Card>
       </div>
+      <h3 className="text-center pt-10">Total Retirement Savings: ${ savings } </h3>
     </main>
   );
 };
