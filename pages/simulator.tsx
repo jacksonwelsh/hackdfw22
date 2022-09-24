f6b28b7f9d59908fa29fa865ee2cd2ee1a1f8708
Rocko Bishop <deck@RockoDeck.event.rightround.com>
2022-09-24 11:31:51 -0500

    Added border color to sim cards.

--- pages/simulator.tsx
+++ pages/simulator.tsx
@@ -10,17 +10,17 @@ const Simulator: NextPage = () => {
           Explore various life events and experience how they may affect your retirement!
         </h2>
       </div>
-      <div className="grid grid-cols-2 gap-4 my-4 px-10">
-        <Card>
+      <div className="grid grid-cols-2 gap-4 my-4 px-10 ">
+        <Card className="text-center border-2 border-violet-700">
           <h2>Buy a Car.</h2>
         </Card>
-        <Card>
+        <Card className="text-center border-2 border-violet-700">
           <h2>Deposit into 401k.</h2>
         </Card>
-        <Card>
+        <Card className="text-center border-2 border-violet-700">
           <h2>Pay Credit Card debt.</h2>
         </Card>
-        <Card>
+        <Card className="text-center border-2 border-violet-700">
           <h2>Hire an Escort. (sus)</h2>
         </Card>
       </div>
