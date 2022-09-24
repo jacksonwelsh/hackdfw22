71999666df4deac8f5b046166b08ad8c58b97e20
Rocko Bishop <deck@RockoDeck.event.rightround.com>
2022-09-24 16:06:47 -0500

    Got savings functions to work async with useEffect

--- pages/simulator.tsx
+++ pages/simulator.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "../components/Card";
 
 const Simulator: NextPage = () => {
   const [savings, calcSavings] = useState(0)
   const [retSavings, calcRet] = useState(0)
+
+  useEffect(() => {
+    if (savings < 0) {
+      calcRet(0)
+    }
+    else {
+      calcRet(Math.round(savings * 1.07 ** (40) * (1e2)) / 1e2)
+    }
+  }, [savings])
 
   return (
     <main className="flex-col">
@@ -16,16 +25,16 @@ const Simulator: NextPage = () => {
       </div>
       <div className="grid grid-cols-2 gap-4 my-4 px-10 text-center">
         <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
-          <h2 onClick={() => { calcSavings(savings - 20000); calcRet(Math.round(savings * 1.07 ** (40) * (1e2)) / 1e2); }}>Buy a Car.</h2>
+          <h2 onClick={() => calcSavings(savings - 20000)}>Buy a Car.</h2>
         </Card>
         <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
-          <h2 onClick={() => { calcSavings(savings + 200); calcRet(Math.round(savings * 1.07 ** (40) * (1e2)) / 1e2); }}>Deposit $200 into 401k.</h2>
+          <h2 onClick={() => calcSavings(savings + 200)}>Deposit $200 into 401k.</h2>
         </Card>
         <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
-          <h2 onClick={() => { calcSavings(savings - 3000); calcRet(Math.round(savings * 1.07 ** (40) * (1e2)) / 1e2); }}>Pay Credit Card debt.</h2>
+          <h2 onClick={() => calcSavings(savings - 5000)}>Pay Credit Card debt.</h2>
         </Card>
         <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
-          <h2 onClick={() => { calcSavings(savings + 2000); calcRet(Math.round(savings * 1.07 ** (40) * (1e2)) / 1e2); }}>Put $2k into a HYSA.</h2>
+          <h2 onClick={() => calcSavings(savings + 2000)}>Put $2k into a HYSA.</h2>
         </Card>
       </div>
       <h3 className="text-center">Total Capital:</h3>
