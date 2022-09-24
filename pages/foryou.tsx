de777260863f3a66b366cd4ecb03f4b066de1a40
Austin Leath <austinleath18@gmail.com>
2022-09-24 11:26:45 -0500

    change to foryou

---
+++ pages/foryou.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+
+const ForYou: NextPage = () => {
+  //return a tailwind css mobile responsive stock ForYou page with a chart
+  return (
+    <>  </>
+    //write a compoent
+  );
+  };
+export default ForYou;
