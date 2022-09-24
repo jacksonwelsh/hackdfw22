fbb26d44a48755f539dd5f0723479c1a8d18ff38
Jackson Welsh <me@jacksonwel.sh>
2022-09-24 00:54:41 -0500

    very much timeline

---
+++ components/timeline/Segment.tsx
@@ -0,0 +1,26 @@
+interface SegmentProps {
+  children: React.ReactNode;
+  title: string;
+  icon?: React.ReactNode;
+  year: number;
+}
+
+const Segment: React.FC<SegmentProps> = ({ children, title, icon, year }) => {
+  return (
+    <li className="mb-10 ml-6">
+      <span className="flex absolute -left-4 justify-center items-center w-8 h-8 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900 text-blue-200 fill-current">
+        {icon}
+      </span>
+
+      <h2 className="mb-1 ml-2 mt-6 -translate-y-4">
+        <div className="text-sm font-semibold tracking-widest text-gray-300 -mb-1">
+          {year}
+        </div>
+        {title}
+      </h2>
+      <div className="ml-2">{children}</div>
+    </li>
+  );
+};
+
+export default Segment;
