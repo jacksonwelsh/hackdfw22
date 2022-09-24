e166a72e4a9903555628609a8d767261de395a18
Matthew Stieferman <matthewstieferman@my.unt.edu>
2022-09-24 00:10:17 -0500

    fines start ?

---
+++ pages/fines.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Card from "../components/Card";
+
+const Fines: NextPage = () => {
+    
+    return null
+}
+
+export default Fines;
