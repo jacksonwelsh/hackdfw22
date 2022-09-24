a557ca46f1b1336e5242ec3cd8ad0a1712e3ba1d
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2022-09-24 14:48:24 -0500

    Fix hydration error on shavings page

--- pages/shavings.tsx
+++ pages/shavings.tsx
@@ -1,14 +1,46 @@
 import type { NextPage } from "next";
-import { useId } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Card from "../components/Card";
 import companies from "../assets/companies.json";
 
-const Shavings: NextPage = () => {
+interface ShavingsProps {
+  cards: {
+    id: string;
+    title: string;
+    subtitle: string;
+  }[]
+}
+
+const Shavings: NextPage<ShavingsProps> = ({cards}) => {
+
+  return (
+    <main className="flex-col">
+      <div className="text-center pb-10">
+        <h1>Shavings</h1>
+      </div>
+      <div className="grid grid-cols-1 gap-4 my-4 px-10">
+        {/* {cardTitles.map((numEntries, index) => (
+          <Card key={uuidv4()}>
+            <h2>{cardTitles[index]}</h2>
+            <h3>{cardSubtitle[index]}</h3>
+          </Card>
+        ))} */}
+        {cards.map(({id, title, subtitle}) => (
+          <Card key={id}>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+        </Card>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export async function getServerSideProps() {
   function getRandom(min: number, max: number): number {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(1 * (max - min + 1)) + min;
   }
 
   function calcRetirementValue(
@@ -30,13 +62,15 @@ const Shavings: NextPage = () => {
   let retirementDollars: number[] = [];
   let cardSubtitle: string[] = [];
 
+  const cards = [];
+
   for (let i = 0; i < numEntries; i++) {
     let cents = 0;
     cents = getRandom(0, 99);
     cents += getRandom(0, 30) * 100;
 
     dollars.push(cents / 100);
-    console.log(companies);
+    // console.log(companies);
     const businessIndex: number = getRandom(0, companies.companies.length - 1);
     const business: string = companies.companies[businessIndex];
     // const business = "PLACEHOLDER";
@@ -54,23 +88,21 @@ const Shavings: NextPage = () => {
     var subtitle: string =
       "That's $" + compounded.toFixed(2) + " in retirement dollars!";
     cardSubtitle.push(subtitle);
+
+    cards.push({
+      id: uuidv4(),
+      title,
+      subtitle
+    })
   }
 
-  return (
-    <main className="flex-col">
-      <div className="text-center pb-10">
-        <h1>Shavings</h1>
-      </div>
-      <div className="grid grid-cols-1 gap-4 my-4 px-10">
-        {cardTitles.map((numEntries, index) => (
-          <Card key={uuidv4()}>
-            <h2>{cardTitles[index]}</h2>
-            <h3>{cardSubtitle[index]}</h3>
-          </Card>
-        ))}
-      </div>
-    </main>
-  );
-};
+  console.log(cards)
+
+  return {
+    props: {
+      cards
+    }, // will be passed to the page component as props
+  }
+}
 
 export default Shavings;
