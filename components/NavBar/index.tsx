d7f89d0dee06a81cb6a67037de007c1071909a18
Austin Leath <austinleath18@gmail.com>
2022-09-24 00:07:48 -0500

    initial layout and NavBar

---
+++ components/NavBar/index.tsx
@@ -0,0 +1,24 @@
+type NavBarProps = {};
+
+const NavBar: React.FC<NavBarProps> = () => {
+
+return (
+//timeline
+
+//find
+
+//simulator
+
+//portfolio
+
+<nav className="w-full">
+    <h1>this is H1</h1>
+</nav>
+
+
+
+);
+
+};
+
+export default NavBar;
