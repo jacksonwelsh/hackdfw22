8a7c185ea923f944f4afa35fab97df265814b8fd
Austin Leath <austinleath18@gmail.com>
2022-09-24 01:21:01 -0500

    style navbar

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,24 +1,60 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+interface RouteProps {
+    route: string;
+    label: string;
+    icon: React.ReactNode;
+}
+
+const ROUTES: RouteProps[] = [
+  {
+    route: '/timeline',
+    label: 'Timeline',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+  },
+  {
+    route: '/trimmings',
+    label: 'Trimmings',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>  
+  },
+  {
+    route: '/simulator',
+    label: 'Simulator',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+  </svg>
+  },
+  {
+    route: '/portfolio',
+    label: 'Portfolio',
+    icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
+  </svg>
+  },
+]
+
 type NavBarProps = {};
 
 const NavBar: React.FC<NavBarProps> = () => {
+const router = useRouter();
 
 return (
-//timeline
-
-//find
-
-//simulator
-
-//portfolio
-
-<nav className="w-full">
-    <h1 className="fixed inset-x-0 bottom-0 text-center">This is the Footer/NavBar</h1>
-</nav>
-
-
-
-);
-
+  <nav className="fixed inset-x-0 bottom-0 h-16 p-4 bg-neutral-800">
+    <div className="max-w-2xl mx-auto h-full flex justify-between items-center">
+      {ROUTES.map(({route, label, icon}) => {
+        const underlineStyle = router.pathname === route ? 'underline underline-offset-4' : ''
+        return (
+          <Link href={route}>
+          <a className={`flex gap-2 items-center ${underlineStyle}`}>{icon}<span className="hidden sm:inline">{label}</span></a>
+        </Link>
+        )
+      })}
+    </div>
+  </nav>)
 };
 
 export default NavBar;
