9031f1e3eb49694383657b13f6c8731f8c4a1b37
Rocko Bishop <deck@RockoDeck.event.rightround.com>
2022-09-24 15:13:07 -0500

    Removed age and created sister functions to show savings potential on sim page.

--- pages/simulator.tsx
+++ pages/simulator.tsx
@@ -5,7 +5,7 @@ import Card from "../components/Card";
 
 const Simulator: NextPage = () => {
   const [savings, calcSavings] = useState(0)
-  const [age, ageUp] = useState(25)
+  const [retSavings, calcRet] = useState(0)
 
   return (
     <main className="flex-col">
@@ -14,28 +14,23 @@ const Simulator: NextPage = () => {
           Explore various life events and experience how they may affect your retirement!
         </h2>
       </div>
-      <div className="text-center">
-        <h3>
-          You are {age} years old.
-        </h3>
-      </div>
       <div className="grid grid-cols-2 gap-4 my-4 px-10 text-center">
         <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
-          <h2 onClick={() => calcSavings(savings - 20000)}>Buy a Car.</h2>
+          <h2 onClick={() => { calcSavings(savings - 20000); calcRet(Math.round(savings * 1.07 ** (40) * (1e2)) / 1e2); }}>Buy a Car.</h2>
         </Card>
         <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
-          <h2 onClick={() => calcSavings(savings + 200)}>Deposit $200 into 401k.</h2>
+          <h2 onClick={() => { calcSavings(savings + 200); calcRet(Math.round(savings * 1.07 ** (40) * (1e2)) / 1e2); }}>Deposit $200 into 401k.</h2>
         </Card>
         <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
-          <h2 onClick={() => calcSavings(savings - 3000)}>Pay Credit Card debt.</h2>
+          <h2 onClick={() => { calcSavings(savings - 3000); calcRet(Math.round(savings * 1.07 ** (40) * (1e2)) / 1e2); }}>Pay Credit Card debt.</h2>
         </Card>
         <Card className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
-          <h2 onClick={() => calcSavings(savings + 2000)}>Put $2k into a HYSA.</h2>
+          <h2 onClick={() => { calcSavings(savings + 2000); calcRet(Math.round(savings * 1.07 ** (40) * (1e2)) / 1e2); }}>Put $2k into a HYSA.</h2>
         </Card>
       </div>
-      <h3 className="text-center pt-1 cursor-pointer" onClick={() => { ageUp(age + 1); calcSavings(Math.round(savings * 1.07 * (1e2)) / 1e2); }}>&rarr; Age +1 year! &larr;</h3>
-      <h3 className="text-center pt-1">Total Retirement Savings:</h3>
-      <h3 className="text-center">${savings}</h3>
+      <h3 className="text-center">Total Capital:</h3>
+      <h3 className="text-center">Current Savings &rarr; ${savings}</h3>
+      <h3 className="text-center">Retirement Savings &rarr; ${retSavings}</h3>
     </main>
   );
 };
