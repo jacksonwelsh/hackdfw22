eab2d516f42dca6720082e90e9df3beb2604cf96
Rocko Bishop <deck@RockoDeck.event.rightround.com>
2022-09-24 01:31:28 -0500

    Changed hrefs to use Links for smoother navigability

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import Card from "../components/Card";
 
 const Home: NextPage = () => {
@@ -26,12 +27,14 @@ const Home: NextPage = () => {
         <p className="">when the ___ is ___</p>
 
         <div className="grid grid-cols-2 gap-4 my-4">
-          <a href="/timeline" className="">
+          <Link href={"/timeline"}>
+            <a>
             <Card>
               <h2>Timeline &rarr;</h2>
               <p>View your personalized financial Timeline!</p>
-            </Card>
-          </a>
+              </Card>
+            </a>
+          </Link>
 
           <a href="https://nextjs.org/learn" className="">
             <Card>
@@ -49,18 +52,16 @@ const Home: NextPage = () => {
               <p>Discover and deploy boilerplate example Next.js projects.</p>
             </Card>
           </a>
-
-          <a
-            href="/simulator"
-            className=""
-          >
-            <Card>
-              <h2>Financial Simulator &rarr;</h2>
-              <p>
-                Try our fun financial life simulator if you dare! &rarr;
-              </p>
-            </Card>
-          </a>
+          
+          <Link href={"/simulator"}>
+            <a>
+              <Card>
+                <h2>Financial Simulator &rarr;</h2>
+                <p>
+                  Try our fun financial life simulator if you dare! &rarr;
+                </p>
+            </Card></a>
+          </Link>            
         </div>
       </main>
 
