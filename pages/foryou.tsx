a3a7b214a9fbc70a873e2507f7777a658dd548c4
Rocko Bishop <deck@RockoDeck.event.rightround.com>
2022-09-25 01:15:27 -0500

    Added insight messages for Apple, Microsoft, and Amazon.

--- pages/foryou.tsx
+++ pages/foryou.tsx
@@ -28,7 +28,8 @@ const positions: PositionCardProps[] = [
     timeframe: "Daily",
     price: 150.43,
     data: labels.map(() => faker.datatype.number({ min: 148.56, max: 151.47 })),
-    insightText: "Apple is a great company",
+    insightText: "Despite announcing anticipated product lineups during 2022's Keynote event, Apple is underperforming. We recommend keeping an eye on this position,\
+    as it is unknown whether prices will continue to drop or if they will recover. If value continues to decline, we may suggest moving your funds to a more stable position.",
   },
   {
     id: uuidv4(),
@@ -37,9 +38,10 @@ const positions: PositionCardProps[] = [
     trend: "down",
     percentage: -1.27,
     timeframe: "Daily",
-    price: 237.92 ,
+    price: 237.92,
     data: labels.map(() => faker.datatype.number({ min: 235.20, max: 241.13 })),
-    insightText: "Microsoft is a great company",
+    insightText: "Microsoft is down today. Recent statistics and trends have shown a steep decline in value -- over $65 in the last six months. Microsoft has begun to \
+    listen less to its consumer base, meaning overall opinion of the company is down. When opinion is down, so is value. We recommend selling and buying a more stable position.",
   },
   {
     id: uuidv4(),
@@ -50,7 +52,8 @@ const positions: PositionCardProps[] = [
     timeframe: "Daily",
     price: 113.78,
     data: labels.map(() => faker.datatype.number({ min: 112.06, max: 116.05 })),
-    insightText: "Amazon is a great company",
+    insightText: "Amazon is down today, but recent trends show that the position is quite stable. Amazon continues to innovate and improve, so we firmly believe the price \
+    will recover. No need to be concerned, we recommend keeping this position for now.",
   },
 ];
 
@@ -64,9 +67,9 @@ const ForYou: NextPage = () => {
         <h2 className="text-lg font-bold mt-0">Personalized insights at a glance</h2>
       </FadeInFromTop>
       <div className="grid grid-cols-1 gap-4 w-full place-items-center">
-      {positions.map((position, index) => (
-        <PositionCard key={position.id} {...position} />
-      ))}
+        {positions.map((position, index) => (
+          <PositionCard key={position.id} {...position} />
+        ))}
       </div>
     </div>
   );
