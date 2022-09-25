8a2f30b466e9a603525dd8bcdfd091a0cd017b65
Rocko Bishop <deck@RockoDeck.event.rightround.com>
2022-09-24 20:29:50 -0500

    Refactored savings totals and added explanation text.

--- pages/simulator.tsx
+++ pages/simulator.tsx
@@ -40,15 +40,24 @@ const Simulator: NextPage<SimulatorProps> = (props) => {
       <div className="grid grid-cols-2 gap-4 my-4 px-10 text-center">
         {cells.map((cell: SimulatorCell) => {
           return (
-            <Card key={cell.id} className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900">
-              <h3 onClick={() => onCellClick(savings + cell.cost)}>{cell.name}</h3>
+            <Card key={cell.id} className="border-2 border-violet-700 bg-gradient-to-tr from-blue-500 to-purple-500 text-stone-900 text-xl flex justify-center items-center">
+              <p onClick={() => onCellClick(savings + cell.cost)}>{cell.name} ({cell.cost.toLocaleString('en-US', { style: 'currency', currency: 'USD' })})</p>
             </Card>
           )
         })}
       </div>
-      <h3 className="text-center">Total Capital:</h3>
-      <h3 className="text-center">Current Savings &rarr; {savings.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</h3>
-      <h3 className="text-center">Retirement Savings &rarr; {retSavings.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</h3>
+      <div className="mx-10 text-center">
+        <h4>At 25, you may not have much money in savings. If you invest early, even a small amount of money can make a difference.
+          In 40 years your assets could be worth exponentially more!
+        </h4>
+      </div>
+      <div className="fixed inset-x-0 bottom-20">
+        <h3 className="text-center">Total Earnings:</h3>
+        <h3 className="text-center">Current Savings &rarr;</h3>
+        <h3 className='text-center'>{savings.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</h3>
+        <h3 className="text-center">Retirement Savings &rarr;</h3>
+        <h3 className='text-center'>{retSavings.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</h3>
+      </div>
     </main>
   );
 };
